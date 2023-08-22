e71c916d10870f321fa4db4f5f9258a39a1f5d04
cjrace <52536248+cjrace@users.noreply.github.com>
2023-08-22 08:56:30 +0100

    Update dashboard_panels.R
    Update broken link (on behalf of team)

--- R/dashboard_panels.R
+++ R/dashboard_panels.R
@@ -128,7 +128,7 @@ homepage_panel <- function() {
                                   Successful applicants will access the data through the ONS Secure Research Service (SRS).", tags$br(), tags$br(),
                           "For more information, please see the previously published",
                           tags$a(
-                            href = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1059557/Education_children_s_social_care_and_offending_descriptive_stats_Technical_Note_FINAL.pdf",
+                            href = "https://www.gov.uk/government/publications/education-childrens-social-care-and-offending",
                             "technical note"
                           ), "."
                         )
